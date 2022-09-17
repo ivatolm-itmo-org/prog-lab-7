ed1d4b43227a48c126577e34c834938e27a62b80
Ivan Tolmachev <ivatolm@gmail.com>
2022-09-17 14:12:36 +0300

    lab 1 bpa improve

--- bpa/1/lab0_4.sh
+++ bpa/1/lab0_4.sh
@@ -1,19 +1,12 @@
-# 1
-wc -l `grep -r "" lab0 2>&1 | grep -o ".*:" | grep -o "[/_0-9a-zA-Z]*" | grep "/m[_0-9a-zA-Z]*$"` | sort
+wc -l `grep --include="m*" -rl "" lab0` 2>&1 | sort
 
-# 2
-ls -lc `grep -r "" lab0 2> /dev/null | grep -o ".*:" | grep -o "[/_0-9a-zA-Z]*" | grep "/m[_0-9a-zA-Z]*$"` | head -n 4
+ls -lc `grep --include="m*" -srl "" lab0` | head -n 4
 
-# 3
-# there is not files like this, but cmd is:
-# cat -n `grep -r "" lab0 2>&1 | grep -o ".*:" | grep -o "[/_0-9a-zA-Z]*" | grep "/[_0-9a-zA-Z]*6$" | sort -r`
+# cat -n `grep --include="*6" -rl "" lab0` 2>&1 | sort -r
 
-# 4
-ls lab0/mightyena8 | sort -r
+ls -p lab0/mightyena8 | grep -v / | sort -r
 
-# 5
 ls -lcr lab0/metagross6 2>/tmp/some_error_file
 rm /tmp/some_error_file
 
-# 6
-ls -l `grep -r "" lab0/starmie6 | grep -o ".*:" | grep -o "[/_0-9a-zA-Z]*" 2>/dev/null` | sort -n -k 2 -r
+ls -l `grep --include="*" -srl "" lab0/starmie6` | sort -n -k 2 -r
