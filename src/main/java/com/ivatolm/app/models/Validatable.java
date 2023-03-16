71282add164fe9a5dfdad391fcefba315b103b68
Ivan Tolmachev <ivatolm@gmail.com>
2023-03-16 10:47:46 +0300

    Add docs for Validatable interface

--- src/main/java/com/ivatolm/app/models/Validatable.java
+++ src/main/java/com/ivatolm/app/models/Validatable.java
@@ -1,7 +1,17 @@
 package com.ivatolm.app.models;
 
+/**
+ * Interface for validating objects.
+ *
+ * @author ivatolm
+ */
 public interface Validatable {
 
+    /**
+     * Check object for validity.
+     *
+     * @return true if object is valid, else false
+     */
     boolean validate();
 
 }
